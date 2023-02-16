f4d878173f9a8970901b3c8a68a473297849ef2d
RSSChenChu <shenchu.jin@edu.uah.es>
2023-02-16 16:36:43 +0100

    Esto si que no me lo esperaba

--- lab02-draw/prueba.py
+++ lab02-draw/prueba.py
@@ -8,13 +8,17 @@ arcade.start_render()
 
 arcade.draw_point(50, 300, arcade.color.RED, 50)
 
-arcade.draw_line(75, 300, 300, 300, arcade.color.RED,10)
+arcade.draw_line(75, 300, 300, 300, arcade.color.WHITE, 10)
 
 texture = arcade.load_texture(":resources:images/space_shooter/playerShip1_orange.png")
 scale = .6
-arcade.draw_scaled_texture_rectangle(540, 120, texture, scale, 0)
-arcade.draw_scaled_texture_rectangle(540, 60, texture, scale, 45)
+arcade.draw_scaled_texture_rectangle(300, 300, texture, scale, 90)
 
-arcade.finish_render()
+arcade.draw_rectangle_filled(350, 400, 100, 10, arcade.color.RED_VIOLET)
+arcade.draw_rectangle_filled(350, 200, 100, 10, arcade.color.RED_VIOLET)
+
+arcade.draw_arc_outline(150, 81, 15, 36, arcade.color.BRIGHT_MAROON, 90, 360)
+
+arcade.draw_circle_outline(300, 300, 30, arcade.color.ORANGE, 72,  90, 360)
 
 arcade.run()
